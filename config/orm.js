a553f20901cf5fc88da7f855cd4c3fc3446adeb0
arbdt <67365149+arbdt@users.noreply.github.com>
2020-11-16 16:58:28 +1030

    added selectAll function with select * from query to ORM file

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,28 @@
 // import modules -----
 let connection = require("./connection");
 
-// ORM commands -----
+// create ORM object and commands -----
+let orm = {
 // select all
-
+selectAll: function(tableName, callback){
+    // run select all query from specified table
+    let queryString = `SELECT * FROM ??`;
+    connection.query(queryString, [tableName],function(error, result){
+        if (error){
+            throw error;
+        } else{
+            // send result to callback function for later use
+            callback(result);
+        }
+    });
+},
 // insert one
-
+insertOne: function(){
+},
 // update one
+updateOne: function(){
+}
+};
+
+// export ORM
+module.exports = orm;
