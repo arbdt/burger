025ee5b110685d6b9bc906daa8fab80f2e271c0d
arbdt <67365149+arbdt@users.noreply.github.com>
2020-11-17 16:20:22 +1030

    added .get'/' route to burger controller that connects to burgers.getAllBurgers function

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,7 +5,25 @@ let burger = require("../models/burger");
 // create router
 let router = express.Router();
 
-// define router
+// define router ----
+// GET all burgers
+router.get("/", function(request, response){
+    // call burger get all
+    burger.getAllBurgers(function(data){
+        // send burgers to view renderer
+        response.render("index",{burgers: data});
+    });
+});
+
+// POST new burger
+router.post("/api/burgers", function(request, response){
+
+});
+
+// PUT burger status
+router.put("/api/burgers/:id"), function(request, response){
+
+};
 
 // export router
 module.exports = router;
