46c0b16c479202d794954162ab8259754ccc0a45
arbdt <67365149+arbdt@users.noreply.github.com>
2020-11-17 15:45:56 +1030

    added getALLBurgers function to Burgers.js model that cionnects to ORM selectALL function

--- models/burger.js
+++ models/burger.js
@@ -1,6 +1,27 @@
-// import modules
-// imprt ORM
+// import ORM
+let orm = require("../config/orm");
 
-// burger ORM functions
+// burger object definition
+let burger = {
+// burger ORM functions -----
+// all burgers
+getAllBurgers(callback){
+    // refer to ORM "selectAll" function
+    orm.selectAll("burgers", function(output){
+        callback(output);
+    });
+},
+// insert burger
+createBurger(columnArray, valueArray, callback){
+    orm.insertOne("burgers", columnArray, valueArray, function(output){
+        callback(output);
+    });
+},
+// change burger details
+updateBurger(column, conditions, callback){
+    orm.updateOne("burgers", changedValues, conditions);
+}
+};
 
 // export burger
+module.exports = burger;
