337722aedd30cd54dde58128a76d841a904685df
arbdt <67365149+arbdt@users.noreply.github.com>
2020-11-18 14:23:44 +1030

    set up server.js with express app and router and handlebars

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
-// import modules
+// import modules -----
 let express = require("express");
+let exphbs = require("express-handlebars");
+var router = require("./controllers/burgers_controller.js");
+
+// set up express app instance -----
+const PORT = process.env.PORT || 8080;
+let app = express();
+// set up to use routes from controller
+app.use(router);
+// parse requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// static link for css file
+app.use(express.static("public"));
+
+// link in handlebars view engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// launch server -----
+app.listen(PORT, function() {
+  console.log(`Server listening on port ${PORT}`);
+});
