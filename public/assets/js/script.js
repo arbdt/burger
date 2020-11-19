f4684152415128118a8e9c84eb06760fec7a588a
arbdt <67365149+arbdt@users.noreply.github.com>
2020-11-19 19:50:17 +1030

    added custom client side script file for use with page buttons

---
+++ public/assets/js/script.js
@@ -0,0 +1,47 @@
+// SCRIPT FOR JQUERY CLICK AND AJAX FOR CLIENT SIDE
+$(document).ready(function(){
+
+    // ajax for sending POST for create new burger
+
+    // ajax PUT to set burger to DEVOURED=TRUE; -----
+    // button click
+    $(".eatBurgerBtn").on("click", function(event){
+        // get properties from page item
+        let idAffected = $(this).data("id");
+        let setDevoured = {devoured: true};
+        console.log(`Burger ID: ${idAffected}; ` + JSON.stringify(setDevoured));
+        let putUrl = "/api/burgers/" + idAffected;
+        // send to ajax PUT
+        $.ajax({
+            url: putUrl,
+            type: "PUT",
+            data: setDevoured
+        }).then(
+            function() {
+            console.log("A burger has been devoured");
+            // refresh page to re-GET the burger lists
+            location.reload();
+        });
+    });
+
+    // ajax for set burger to DEVOURED=FALSE;
+    // button click
+    $(".regurgitateBurgerBtn").on("click", function(event){
+        // get properties from page item
+        let idAffected = $(this).data("id");
+        let setDevoured = {devoured: false};
+        console.log(`Burger ID: ${idAffected}; ` + JSON.stringify(setDevoured));
+        let putUrl = "/api/burgers/" + idAffected;
+        // send to ajax PUT
+        $.ajax({
+            url: putUrl,
+            type: "PUT",
+            data: setDevoured
+        }).then(
+            function() {
+            console.log("A burger has been devoured");
+            // refresh page to re-GET the burger lists
+            location.reload();
+        });
+    });
+});
