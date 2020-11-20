db9f4368b0b36554b1da747947371309231429da
arbdt <67365149+arbdt@users.noreply.github.com>
2020-11-20 16:41:18 +1030

    adjusted updateBurger function to include callback

--- models/burger.js
+++ models/burger.js
@@ -3,28 +3,30 @@ let orm = require("../config/orm");
 
 // burger object definition
 let burger = {
-// burger ORM functions -----
-// all burgers
-getAllBurgers(callback){
-    // refer to ORM "selectAll" function
-    orm.selectAll("burgers", function(output){
-        // send output to callback
-        callback(output);
-    });
-},
-// insert burger
-createBurger(columnArray, valueArray, callback){
-    // call ORM insert function
-    orm.insertOne("burgers", columnArray, valueArray, function(output){
-        // send output to callback
-        callback(output);
-    });
-},
-// change burger details
-updateBurger(column, conditions, callback){
-    orm.updateOne("burgers", changedValues, conditions);
+    // burger ORM functions -----
+    // all burgers
+    getAllBurgers: function (callback){
+        // refer to ORM "selectAll" function
+        orm.selectAll("burgers", function(output){
+            // send output to callback
+            callback(output);
+        });
+    },
+    // insert burger
+    createBurger: function (columnArray, valueArray, callback){
+        // call ORM insert function
+        orm.insertOne("burgers", columnArray, valueArray, function(output){
+            // send output to callback
+            callback(output);
+        });
+    },
+    // change burger details
+    updateBurger: function (columnArray, valueArray, conditions, callback){
+        orm.updateOne("burgers", columnArray, valueArray, conditions, function(output){
+            callback(output);
+        });
+    }
 }
-};
 
 // export burger
 module.exports = burger;
