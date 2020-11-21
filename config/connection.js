da1afaaad3ce986d0160de92c31ae0f2bb767d0d
arbdt <67365149+arbdt@users.noreply.github.com>
2020-11-21 20:12:11 +1030

    added connection for heroku jawsDB

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,16 @@
 const mysql = require("mysql");
 
 // set up connection -----
-let connection = mysql.createConnection({
+let connection;
+
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else { connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: ""
+    password: "",
+    database: "burgers_db"
 });
 // connect to database
 connection.connect(function (error){
